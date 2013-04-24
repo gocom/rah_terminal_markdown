12507cfdf2d1e0ee86247ee673be34c279992aaf
Jukka Svahn <web@rahinaa.biz>
2013-04-24 17:30:13 +0300

    Removed requirements notices from source.

--- markdown.php
+++ markdown.php
@@ -8,9 +8,6 @@
  * @license   GNU GPLv2
  * @link      https://github.com/gocom/rah_terminal_markdown
  *
- * Requires PHP Markdown
- * http://michelf.com/projects/php-markdown/
- *
  * Copyright (C) 2013 Jukka Svahn http://rahforum.biz
  * Licensed under GNU Genral Public License version 2
  * http://www.gnu.org/licenses/gpl-2.0.html
